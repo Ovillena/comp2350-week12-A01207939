ac4fa7ab4d1d157e94e17cbdc5601b9d2d8df2cf
Octavio Villena <octaviovillena@hotmail.com>
2021-04-15 22:09:46 -0700

    connection, routes, pet.ejs

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,18 +1,21 @@
-const MongoClient = require("mongodb").MongoClient;
-
+const database = require("mongoose");
 const is_heroku = process.env.IS_HEROKU || false;
-
-
-const herokuURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
-
-const localURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
-
+const databaseName = "lab_example";
+const herokuURI =
+  "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.n1i6g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+databaseName + "?retryWrites=true&w=majority";
+const localURI =
+  "mongodb://localhost/" + databaseName + "?authSource=admin&retryWrites=true;";
 if (is_heroku) {
-	var database = new MongoClient(herokuURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
-else {
-	var database = new MongoClient(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
+  database.connect(herokuURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} else {
+  database.connect(localURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 module.exports = database;
-		
